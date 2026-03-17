f8d348e6588cbafae67fd16985b08d55af27f9f5
Nguyen Nho Dung <nndungcse@gmail.com>
2026-03-17 21:41:01 +0700

    Initial source code

---
+++ tests/login.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Login feature", () => {
+  test("login success", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+
+    await page.locator("#username").fill("tomsmith");
+
+    await page.locator("#password").fill("SuperSecretPassword!");
+
+    await page.locator('button[type="submit"]').click();
+
+    await expect(page.locator("#flash")).toContainText(
+      "You logged into a secure area!",
+    );
+  });
+});
